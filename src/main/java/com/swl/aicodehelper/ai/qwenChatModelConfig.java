7f32c32e58b3061dbfcf1fc0fef5a71c3fd6e78f
1350147451@qq.com <1350147451@qq.com>
2026-04-24 21:33:39 +0800

    Initial commit

---
+++ src/main/java/com/swl/aicodehelper/ai/qwenChatModelConfig.java
@@ -0,0 +1,4 @@
+package com.swl.aicodehelper.ai;
+
+public class qwenChatModelConfig {
+}
---
+++ src/test/java/com/swl/aicodehelper/AiCodeHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swl.aicodehelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
