a478624d9f1b4e8d9d6eacdb27c6462486539dae
1350147451@qq.com <1350147451@qq.com>
2026-04-26 23:33:42 +0800

    可观测性

--- src/main/java/com/swl/aicodehelper/ai/qwenChatModelConfig.java
+++ src/main/java/com/swl/aicodehelper/ai/qwenChatModelConfig.java
@@ -2,21 +2,28 @@ package com.swl.aicodehelper.ai;
 
 import dev.langchain4j.community.model.dashscope.QwenChatModel;
 import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.listener.ChatModelListener;
+import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class qwenChatModelConfig {
 
     @Value("${langchain4j.community.dashscope.chat-model.api-key}")
     private String dashscopeApiKey;
+    @Resource
+    private ChatModelListener chatModelListener;
 
     @Bean
     public ChatModel qwenMaxChatModel() {
         ChatModel qwenChatModel = QwenChatModel.builder()
                 .apiKey(dashscopeApiKey)
                 .modelName("qwen-max")
+                .listeners(List.of(chatModelListener))
                 .build();
         return qwenChatModel;
     }
